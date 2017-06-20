bf6fd76916eb6cf3a60c9f63d9cb0270ffba06b7
najeira <najeira@gmail.com>
2017-06-20 18:46:40 +0900

    implement twitter_sign_in
    it work in my environment!

--- twitter_sign_in/ios/Classes/TwitterSignInPlugin.m
+++ twitter_sign_in/ios/Classes/TwitterSignInPlugin.m
@@ -1,20 +1,55 @@
 #import "TwitterSignInPlugin.h"
+#import <TwitterKit/TwitterKit.h>
+
+@interface NSError (FlutterError)
+@property(readonly, nonatomic) FlutterError *flutterError;
+@end
+
+@implementation NSError (FlutterError)
+- (FlutterError *)flutterError {
+  return [FlutterError errorWithCode:[NSString stringWithFormat:@"Error %ld", (long)self.code]
+                             message:self.domain
+                             details:self.localizedDescription];
+}
+@end
 
 @implementation TwitterSignInPlugin
+
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
   FlutterMethodChannel* channel = [FlutterMethodChannel
       methodChannelWithName:@"twitter_sign_in"
             binaryMessenger:[registrar messenger]];
   TwitterSignInPlugin* instance = [[TwitterSignInPlugin alloc] init];
+  [registrar addApplicationDelegate:instance];
   [registrar addMethodCallDelegate:instance channel:channel];
 }
 
 - (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
   if ([@"getPlatformVersion" isEqualToString:call.method]) {
     result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else if ([call.method isEqualToString:@"init"]) {
+    [[Twitter sharedInstance] startWithConsumerKey:call.arguments[@"consumer"] consumerSecret:call.arguments[@"secret"]];
+    result(nil);
+  } else if ([call.method isEqualToString:@"signIn"]) {
+    [[Twitter sharedInstance] logInWithCompletion:^(TWTRSession *session, NSError *error) {
+        if (error) {
+          result(error.flutterError);
+        } else {
+          if (session) {
+            result(@{@"authToken": session.authToken,
+              @"authTokenSecret": session.authTokenSecret,
+              @"userName": session.userName,
+              @"userID": session.userID});
+          }
+        }
+    }];
   } else {
     result(FlutterMethodNotImplemented);
   }
+}
+
+- (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary *)options {
+  return [[Twitter sharedInstance] application:app openURL:url options:options];
 }
 
 @end
--- twitter_sign_in/lib/twitter_sign_in.dart
+++ twitter_sign_in/lib/twitter_sign_in.dart
@@ -3,9 +3,44 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 
 class TwitterSignIn {
-  static const MethodChannel _channel =
-      const MethodChannel('twitter_sign_in');
+  static const MethodChannel _channel = const MethodChannel('twitter_sign_in');
+  
+  static Future<String> get platformVersion => _channel.invokeMethod('getPlatformVersion');
+  
+  // Start Twitter with your consumer key and secret.
+  // These will override any credentials present in your applications Info.plist.
+  // You do not need to call this method unless you wish to provide credentials other than those
+  // in your Info.plist.
+  static Future<Null> init(String consumer, String secret) async {
+    return _channel.invokeMethod('init', <String, String>{"consumer": consumer, "secret": secret});
+  }
+  
+  // Triggers user authentication with Twitter.
+  // This method will present UI to allow the user to log in if there are no saved Twitter login credentials.
+  static Future<TwitterSession> signIn() async {
+    final Map<String, dynamic> data = await _channel.invokeMethod('signIn');
+    var ts = new TwitterSession._(data);
+    return ts;
+  }
+  
+  static TwitterSession _currentSession;
+  
+  // gets the cached current session, or `null` if there is none.
+  static TwitterSession get currentSession => _currentSession;
+}
 
-  static Future<String> get platformVersion =>
-      _channel.invokeMethod('getPlatformVersion');
+class TwitterSession {
+  final Map<String, dynamic> _data;
+  
+  TwitterSession._(this._data);
+
+  String get authToken => this._data['authToken'];
+  String get authTokenSecret => this._data['authTokenSecret'];
+  String get userName => this._data['userName'];
+  String get userID => this._data['userID'];
+
+  @override
+  String toString() {
+    return '$runtimeType($_data)';
+  }
 }
